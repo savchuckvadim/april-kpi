16e8f75c884a535b3c3d50cacf4416abb83b3533
savchuckvadim <savchuckvadim@gmail.com>
2022-05-06 03:48:39 +0300

    add mui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './modules/assets/logo.jpg';
+
 import './App.css';
 import GetSheetsContainer from './modules/components/Get-Sheets-Container';
+import BasicTable from './modules/components/Table/Table';
+import Header from './modules/components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+     <Header/>
       <GetSheetsContainer />
+      <BasicTable />
     </div>
   );
 }
