2f9d167779bdcd4c7a336bca307dfe3fa5d9b4ce
savchuckvadim <savchuckvadim@gmail.com>
2022-05-08 13:22:34 +0300

    statistics-area layout

--- src/modules/components/Header/Header.jsx
+++ src/modules/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 
-import logo from '../../assets/logo.jpg';
+import logo from '../../assets/logo.svg';
 import '../../../App.css';
 import './Header.css'
 import ManagerFilterContainer from './Manager-Filter/Manager-Filter-Container';
